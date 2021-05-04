a2b50a09ee74682010b95fc7ddb7197ebe3b64b7
LaraCassandra <190218@virtualwindow.co.za>
2021-05-04 21:11:18 +0200

    Parse settings.html

--- Project/Program.cs
+++ Project/Program.cs
@@ -67,6 +67,8 @@ namespace Project
                                 data = Encoding.UTF8.GetBytes(IndexHtmlParser.Process(input));
                             else if (path == "/inventory.html")
                                 data = Encoding.UTF8.GetBytes(InventoryHtmlParser.Process(input));
+                            else if (path == "/settings.html")
+                                data = Encoding.UTF8.GetBytes(SettingsHtmlParser.Process(input));
                             else 
                                 throw new FileNotFoundException("Not a page");
 
