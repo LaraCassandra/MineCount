0f8ec8e6bc3b7d7f19092729977292818539e0d7
LaraCassandra <190218@virtualwindow.co.za>
2021-05-18 21:04:11 +0200

    Add stone recipes to recipe book

--- Project/Classes/RecipeBook.cs
+++ Project/Classes/RecipeBook.cs
@@ -31,6 +31,28 @@ namespace Project
             recipes.Add(woodShovelRecipe);
             recipes.Add(woodPickaxeRecipe);
             recipes.Add(woodSwordRecipe);
+
+            // STONE RECIPES
+            Recipe stoneAxeRecipe = new Recipe((Crafted) StoneAxe.Get(), new Block[3,3] {{CobblestoneBlock.Get(), CobblestoneBlock.Get(), null},
+                                                                                    {CobblestoneBlock.Get(), Stick.Get(), null},
+                                                                                    {null, Stick.Get(), null}});
+            
+            Recipe stoneShovelRecipe = new Recipe((Crafted) StoneShovel.Get(), new Block[3,3]{{null, CobblestoneBlock.Get(), null},
+                                                                                            {null, Stick.Get(), null},
+                                                                                            {null, Stick.Get(), null}});
+
+            Recipe stonePickaxeRecipe = new Recipe((Crafted) StonePickaxe.Get(), new Block[3,3]{{CobblestoneBlock.Get(), CobblestoneBlock.Get(), CobblestoneBlock.Get()},
+                                                                                            {null, Stick.Get(), null},
+                                                                                            {null, Stick.Get(), null}});
+
+            Recipe stoneSwordRecipe = new Recipe((Crafted) StoneSword.Get(), new Block[3,3]{{null, CobblestoneBlock.Get(), null},
+                                                                                            {null, CobblestoneBlock.Get(), null},
+                                                                                            {null, Stick.Get(), null}});
+            // ADD STONE RECIPES TO ARRAY
+            recipes.Add(stoneAxeRecipe);
+            recipes.Add(stoneShovelRecipe);
+            recipes.Add(stonePickaxeRecipe);
+            recipes.Add(stoneSwordRecipe);
         }
 
         public static ArrayList Recipes
