863a9dde7df100a18e49ce2bcd7506349568f6da
LaraCassandra <190218@virtualwindow.co.za>
2021-05-18 19:54:47 +0200

    Add wood recipes

--- Project/Classes/RecipeBook.cs
+++ Project/Classes/RecipeBook.cs
@@ -9,10 +9,28 @@ namespace Project
 
         public static void Populate()
         {
-            Recipe axeRecipe = new Recipe((Crafted) WoodAxe.Get(), new Block[3,3] {{WoodBlock.Get(), WoodBlock.Get(), null},
+
+            // WOOD RECIPES
+            Recipe woodAxeRecipe = new Recipe((Crafted) WoodAxe.Get(), new Block[3,3] {{WoodBlock.Get(), WoodBlock.Get(), null},
                                                                                     {WoodBlock.Get(), Stick.Get(), null},
                                                                                     {null, Stick.Get(), null}});
-            recipes.Add(axeRecipe);
+            
+            Recipe woodShovelRecipe = new Recipe((Crafted) WoodShovel.Get(), new Block[3,3]{{null, WoodBlock.Get(), null},
+                                                                                            {null, Stick.Get(), null},
+                                                                                            {null, Stick.Get(), null}});
+
+            Recipe woodPickaxeRecipe = new Recipe((Crafted) WoodPickaxe.Get(), new Block[3,3]{{WoodBlock.Get(), WoodBlock.Get(), WoodBlock.Get()},
+                                                                                            {null, Stick.Get(), null},
+                                                                                            {null, Stick.Get(), null}});
+
+            Recipe woodSwordRecipe = new Recipe((Crafted) WoodSword.Get(), new Block[3,3]{{null, WoodBlock.Get(), null},
+                                                                                            {null, WoodBlock.Get(), null},
+                                                                                            {null, Stick.Get(), null}});
+            // ADD WOOD RECIPES TO ARRAY
+            recipes.Add(woodAxeRecipe);
+            recipes.Add(woodShovelRecipe);
+            recipes.Add(woodPickaxeRecipe);
+            recipes.Add(woodSwordRecipe);
         }
 
         public static ArrayList Recipes
